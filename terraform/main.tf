05150d4cf6ac6adafd5147a78f12c49561621077
nenis11andres <nenis11andres@gmail.com>
2025-04-07 21:22:58 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,16 +35,17 @@ resource "aws_s3_bucket_policy" "policy" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Sid       = "PublicReadGetObject"
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.pagina_ia.arn}/*"
-    }]
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.pagina_ia.arn}/*"
+      }
+    ]
   })
 }
-
 
 output "bucket_name" {
   value = aws_s3_bucket.pagina_ia.bucket
@@ -53,4 +54,3 @@ output "bucket_name" {
 output "website_url" {
   value = aws_s3_bucket.pagina_ia.website_endpoint
 }
-
