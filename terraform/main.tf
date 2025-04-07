f97c37950723b3ce5b5fce3ce9851e5f37d8c93c
nenis11andres <nenis11andres@gmail.com>
2025-04-07 21:07:08 +0200

    Modificacion main.tf por pequeño fallo

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,10 +50,6 @@ output "bucket_name" {
   value = aws_s3_bucket.pagina_ia.bucket
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.pagina_ia.bucket
-}
-
 output "website_url" {
   value = aws_s3_bucket.pagina_ia.website_endpoint
 }
