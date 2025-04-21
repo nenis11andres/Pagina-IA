e9739d3f4bbde771b23f39669af6237f4d947a97
nenis11andres <nenis11andres@gmail.com>
2025-04-21 14:31:12 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket         = "aop-pagina-ia"      # El mismo bucket del backend
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
+# Referencia al bucket creado en backend.tf
 resource "aws_s3_bucket" "pagina_ia" {
-  bucket = var.bucket_name
+  bucket = var.bucket_name  # Este nombre debe coincidir con el que usas en el backend
   tags   = var.tags
 }
 
+# Bloqueo de acceso público para el bucket de la página web
 resource "aws_s3_bucket_public_access_block" "no_block_public_access" {
   bucket = aws_s3_bucket.pagina_ia.id
 
@@ -16,6 +28,7 @@ resource "aws_s3_bucket_public_access_block" "no_block_public_access" {
   restrict_public_buckets = false
 }
 
+# Configuración de la página web estática en el bucket
 resource "aws_s3_bucket_website_configuration" "pagina_ia_website" {
   bucket = aws_s3_bucket.pagina_ia.id
 
@@ -28,6 +41,7 @@ resource "aws_s3_bucket_website_configuration" "pagina_ia_website" {
   }
 }
 
+# Política de acceso público para permitir la lectura de los objetos del bucket
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.pagina_ia.id
 
@@ -45,6 +59,7 @@ resource "aws_s3_bucket_policy" "policy" {
   })
 }
 
+# Subir el archivo index.html al bucket
 resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.pagina_ia.id
   key          = "index.html"
@@ -52,6 +67,7 @@ resource "aws_s3_object" "index" {
   content_type = "text/html"
 }
 
+# Subir el archivo estilos.css al bucket
 resource "aws_s3_object" "css" {
   bucket       = aws_s3_bucket.pagina_ia.id
   key          = "estilos.css"
@@ -59,6 +75,7 @@ resource "aws_s3_object" "css" {
   content_type = "text/css"
 }
 
+# Subir todos los archivos dentro de la carpeta assets al bucket
 resource "aws_s3_object" "assets" {
   for_each     = fileset("${path.module}/../assets", "**")
   bucket       = aws_s3_bucket.pagina_ia.id
@@ -77,6 +94,7 @@ resource "aws_s3_object" "assets" {
   )
 }
 
+# Outputs para el nombre del bucket y la URL del sitio web
 output "bucket_name" {
   value = aws_s3_bucket.pagina_ia.bucket
 }
